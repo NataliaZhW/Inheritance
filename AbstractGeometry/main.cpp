5b7f5ccf0b2d3fae5f4a75c7d7d902ad701a1cf6
user <user@WIN-S3FLAGBG853>
2024-07-15 18:23:45 +0300

    +Circle

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -12,10 +12,14 @@ namespace Geometry
 {
 	enum Color
 	{
-		CONSOLE_BLUE = 0x15,
+		CONSOLE_BLUE = 0x09,
 		CONSOLE_GREEN = 0xAA,
 		CONSOLE_RED = 0xCC,
-		CONSOLE_DEFAULT = 0x07
+		CONSOLE_DEFAULT = 0x07,
+		//RGB_RED = (0xFF0000), //Добавляем RGB цвета
+		//RGB_GREEN = (0x00FF00),
+		//RGB_BLUE = (0xFFFF00),
+		//RGB_WHITE = (0xFFFFFF)
 	};
 
 	class Shape
@@ -75,7 +79,6 @@ namespace Geometry
 			cout << "Длина стороны: " << side << endl;
 			Shape::info();
 		}
-
 	};
 
 	class Rectangle : public Shape
@@ -96,21 +99,32 @@ namespace Geometry
 		void set_height(double height) { this->height = height; }
 
 		double get_perimeter()  const override { return (width + height) * 2; }
-		void draw()const  override
+		void draw()const override
 		{
-			HWND hwnd = GetConsoleWindow();
-			HDC hdc = GetDC(hwnd);
+			HWND hwnd = GetConsoleWindow(); //1)Получаем дескриптор окна консоли. Переменная в которой хранится описание чего-то.
+			//description 
+			//HWND - Handler to Window(обработчик или дескриптор окна)
+			HDC hdc = GetDC(hwnd); //2) получаем контекст устройства (Device context) окна консоли
+			//DC - это то на чем мы будем рисовать
 			HPEN hPen = CreatePen(PS_SOLID, 5, get_Color());
+			//3) Создаем карандаш, Pen рисует контур фигуры
+			//PS_SOLID - сплошная линия
+			//5-толщина линии в пикселях
+			// 4) cоздаем кисточку
 			HBRUSH hBrush = CreateSolidBrush(get_Color());
+			//5) Выбираем чем и на чем рисовать
 			SelectObject(hdc, hPen);
 			SelectObject(hdc, hBrush);
-
-			::Rectangle(hdc, 100, 50, 800, 350);
-
+			//6) Рисуем прямоугольник:
+			::Rectangle(hdc, 250, 50, 400, 150);
+			//чтобы показать что это глобальная функция надо поставить двойное двоеточие без операнда слева.
+			//7) Освобождаем ресурсы:
 			DeleteObject(hPen);
 			DeleteObject(hBrush);
+
+			ReleaseDC(hwnd, hdc);
 		}
-		void draw1()const 
+		/*void draw1()const
 		{
 			HANDLE hConcole = GetStdHandle(STD_OUTPUT_HANDLE);
 			SetConsoleTextAttribute(hConcole, get_Color());
@@ -123,7 +137,7 @@ namespace Geometry
 				cout << endl;
 			}
 			SetConsoleTextAttribute(hConcole, Color::CONSOLE_DEFAULT);
-		}
+		}*/
 		double get_width()const { return width; }
 		double get_height()const { return height; }
 		virtual void info()const
@@ -134,6 +148,46 @@ namespace Geometry
 			Shape::info();
 		}
 	};
+	class Circle : public Shape //производный класс Круг
+	{
+	private:
+		int width{ 10 }; //Ширина, здесь радиус
+	public:
+		// коонструктор, +вызывает конструктор базового класса для унаследованных полей
+		Circle(int width, Color color) : Shape(color) {}
+		~Circle() {}
+		// Геттеры и сеттеры
+		void setWidth(int width) { this->width = width; }
+		int getWidth()const { return width; }
+
+		// Методы
+		void info()const override { Shape::info(); cout << " Circle " << this<< "\n"; }
+		double get_area()const override
+		{
+			return getWidth() * getWidth() * PI;
+		}
+		virtual double get_perimeter()const
+		{
+			return getWidth() * PI * 2;
+		}		
+		void draw()const override
+		{
+			//system("CLS");
+			HWND hwnd = GetConsoleWindow();
+			HDC hdc = GetDC(hwnd);
+			HPEN hPen = CreatePen(PS_SOLID, 9, get_Color());
+			HBRUSH hBrush = CreateSolidBrush(Geometry::Color::CONSOLE_DEFAULT);
+			SelectObject(hdc, hPen);
+			SelectObject(hdc, hBrush);
+			::Ellipse(hdc, 100, 100, 30 * width, 30 * width);
+
+			DeleteObject(hPen);
+			DeleteObject(hBrush); 
+
+			ReleaseDC(hwnd, hdc);
+		}
+	};
+
 
 }
 int main()
@@ -146,6 +200,10 @@ int main()
 	//square1.draw();
 	square1.info();
 
-	Geometry::Rectangle rectangle1(15,8, Geometry::Color::CONSOLE_BLUE);
+	Geometry::Rectangle rectangle1(15, 8, Geometry::Color::CONSOLE_GREEN);
 	rectangle1.info();
+
+	Geometry::Circle circle1(50, Geometry::Color::CONSOLE_RED);
+	circle1.info();
+	//system("pause"); 
 }
